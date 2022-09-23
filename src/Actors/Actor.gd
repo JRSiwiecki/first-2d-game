389dc3fa2ad9d4a47571acd170026f3b3fe7cce3
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-23 15:42:37 -0700

    Implement player motion and gravity

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,2 +1,11 @@
 extends KinematicBody2D
 class_name Actor
+
+export var speed := Vector2(300.0, 1000.0)
+export var gravity := 3000.0
+var velocity := Vector2.ZERO
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity.y = max(velocity.y, speed.y)
+	velocity = move_and_slide(velocity)
