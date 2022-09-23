fe79397e2c1b36dd1697725ff07eb1e196be119f
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-23 16:58:28 -0700

    Implement player input and movement

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed := Vector2(300.0, 1000.0)
+export var speed := Vector2(800.0, 1000.0)
 export var gravity := 3000.0
 var velocity := Vector2.ZERO
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,2 +1,18 @@
 extends Actor
 
+
+func _physics_process(delta):
+	
+	# move right is 0 for no movement, 1 for movement
+	# move left is 0 for no movement, 1 for movement
+	# ex: D is pressed -> move_right (1) - move_left(0) = 1
+	# ex: nothing is pressed -> move_right (0) - move_left(0) = 0
+	# ex: A and D are pressed -> move_right (1) - move_left(1) = 0
+	# ex: A is pressed -> move_right (0) - move_left(1) = -1
+	var direction := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		1.0
+	)
+	
+	velocity = speed * direction
+	velocity = move_and_slide(velocity)
