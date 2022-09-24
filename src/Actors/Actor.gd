8f11ba814ddeab5e3fae8c29b78030c498dcb52e
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-23 17:34:29 -0700

    Add basic jump

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed := Vector2(800.0, 1000.0)
-export var gravity := 3000.0
-var velocity := Vector2.ZERO
+const FLOOR_NORMAL := Vector2.UP
 
-func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity.y = max(velocity.y, speed.y)
+export var speed := Vector2(800.0, 1400.0)
+export var gravity := 4000.0
+
+var velocity := Vector2.ZERO
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,18 +1,32 @@
 extends Actor
 
-
 func _physics_process(delta):
 	
-	# move right is 0 for no movement, 1 for movement
-	# move left is 0 for no movement, 1 for movement
-	# ex: D is pressed -> move_right (1) - move_left(0) = 1
-	# ex: nothing is pressed -> move_right (0) - move_left(0) = 0
-	# ex: A and D are pressed -> move_right (1) - move_left(1) = 0
-	# ex: A is pressed -> move_right (0) - move_left(1) = -1
-	var direction := Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		1.0
-	)
+	var direction := get_direction()
+	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+
+func get_direction() -> Vector2:
 	
-	velocity = speed * direction
-	velocity = move_and_slide(velocity)
+	var new_direction := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
+	
+	print (new_direction)
+	return new_direction
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2
+	) -> Vector2:
+	
+	var new_velocity := linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	
+	
+	return new_velocity
