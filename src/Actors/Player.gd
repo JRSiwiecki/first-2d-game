4b23ecd5efb1175e7a559dec2567b75f060597ff
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-23 17:37:50 -0700

    Add jump strength

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,8 +2,9 @@ extends Actor
 
 func _physics_process(delta):
 	
+	var is_jump_interrupted := Input.is_action_just_released("jump") and velocity.y < 0.0
 	var direction := get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
@@ -12,13 +13,13 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
 	
-	print (new_direction)
 	return new_direction
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
-		speed: Vector2
+		speed: Vector2,
+		is_jump_interrupted: bool
 	) -> Vector2:
 	
 	var new_velocity := linear_velocity
@@ -28,5 +29,7 @@ func calculate_move_velocity(
 	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	
+	if is_jump_interrupted:
+		new_velocity.y = 0.0
 	
 	return new_velocity
