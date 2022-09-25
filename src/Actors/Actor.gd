4480d8031e814091d03a39405373cb9b341824a0
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-24 17:18:49 -0700

    Add enemy movement and add enemies to main level

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -3,7 +3,7 @@ class_name Actor
 
 const FLOOR_NORMAL := Vector2.UP
 
-export var speed := Vector2(800.0, 1400.0)
+export var speed := Vector2(500.0, 1400.0)
 export var gravity := 4000.0
 
 var _velocity := Vector2.ZERO
--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,1 +1,14 @@
 extends "res://src/Actors/Actor.gd"
+
+# called on game start
+func _ready():
+	_velocity.x = -speed.x
+
+func _physics_process(delta):
+	
+	_velocity.y += gravity * delta
+	
+	if is_on_wall():
+		_velocity.x *= -1.0
+	
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
