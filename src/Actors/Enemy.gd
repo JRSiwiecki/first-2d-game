36dbf3a63415e705dbf335f0f2089b388f990d32
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-26 20:34:12 -0700

    Add small jump to player stomp

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -8,6 +8,7 @@ func _ready():
 func _on_StompDetector_body_entered(body):
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
+	get_node("CollisionShape2D").disabled = true
 	queue_free()
 
 func _physics_process(delta):
